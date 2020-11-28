9d107124a22498f44927954031725ec4f1135a16
ekardnam <ekarndam@autistici.org>
2020-11-28 13:52:59 +0000

    Fixes

--- lpdecrypter/analyzers/bigram_analyzer.py
+++ lpdecrypter/analyzers/bigram_analyzer.py
@@ -10,16 +10,17 @@ class BigramAnalyzer(Analyzer):
     """
         Basic bigram frequency analysis
     """
-    def __init__(self, alphabet, is_range_from_zero=True):
+    def __init__(self, alphabet, is_range_from_zero=True, use_decoded_values=True):
         """
             Creates a BigramAnalyzer
-            alphabet is the alphabet to use
-            is_range_from_zero is True if the encoding space is range(0, n)
+                alphabet is the alphabet to use
+                is_range_from_zero is True if the encoding space is range(0, n)
         """
         self.alphabet = alphabet
         self.N = alphabet.get_encoding_space_cardinality()
         self.alphabet_str = alphabet.decode(alphabet.get_encoding_space())
         self.is_range_from_zero = is_range_from_zero
+        self.use_decoded_values = use_decoded_values
         if not is_range_from_zero:
             raise NotImplementedError('In future implementation alphabet could also have an encoding space that isnt like range(0, n)')
             # basically this is handled by creating a 1-1 mapping from range(0, alphabet.get_encoding_space_cardinality()) to the encoding space
@@ -35,18 +36,24 @@ class BigramAnalyzer(Analyzer):
             for i in range(0, len(word) - 1):
                 x = word[i]
                 y = word[i+1]
-                frequencies[x, y] += 1
+                frequencies[y, x] += 1
         return frequencies
 
     def display(self, frequencies, display_method='plot'):
         if display_method == 'plot':
             plt.figure()
             plt.imshow(frequencies)
-            plt.xticks(range(self.N), self.alphabet_str)
-            plt.yticks(range(self.N), self.alphabet_str)
+            plt.xlabel('First letter')
+            plt.ylabel('Second letter')
+            if self.use_decoded_values:
+                plt.xticks(range(self.N), self.alphabet_str)
+                plt.yticks(range(self.N), self.alphabet_str)
+            else:
+                plt.xticks(range(self.N), self.alphabet.get_encoding_space())
+                plt.yticks(range(self.N), self.alphabet.get_encoding_space())
             for x in range(self.N):
                 for y in range(self.N):
-                    plt.annotate(str(frequencies[x][y]), xy=(x, y), horizontalalignment='center', verticalalignment='center')
+                    plt.annotate(str(frequencies[y][x]), xy=(x, y), horizontalalignment='center', verticalalignment='center')
             plt.show()
         elif display_method == 'text':
             print(frequencies)
--- lpdecrypter/analyzers/frequency_analyzer.py
+++ lpdecrypter/analyzers/frequency_analyzer.py
@@ -7,8 +7,9 @@ class FrequencyAnalyzer(Analyzer):
     """
         Basic frequency analysis
     """
-    def __init__(self, alphabet):
+    def __init__(self, alphabet, use_decoded_values=True):
         self.alphabet = alphabet
+        self.use_decoded_values = use_decoded_values
 
     def analyze(self, data):
         data = self.alphabet.encode(data)
@@ -16,12 +17,17 @@ class FrequencyAnalyzer(Analyzer):
         for value in data:
             if value in frequencies:
                 frequencies[value] += 1
-        return {self.alphabet.decode([value]): frequency for value, frequency in frequencies.items()}
+        if self.use_decoded_values:
+            return {self.alphabet.decode([value]): frequency for value, frequency in frequencies.items()}
+        return frequencies
 
     def display(self, frequencies, display_method='plot'):
         if display_method == 'plot':
             plt.figure()
             plt.bar(frequencies.keys(), frequencies.values())
+            plt.xticks(range(self.alphabet.get_encoding_space_cardinality()), frequencies.keys())
+            plt.xlabel('Letter')
+            plt.ylabel('Frequency')
             plt.show()
         elif display_method == 'text':
             print(frequencies)
